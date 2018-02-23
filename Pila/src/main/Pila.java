ea3be6edf709cf6a80d3bac6bf6adb9aef4cb89f
Raul Garcia <33263436+raulgarciadevop@users.noreply.github.com>
2018-02-23 11:03:37 -0700

    Tarea 23-02-2018
    Tarea:
    
    Crear interfaz y tablas, modelo

---
+++ Pila/src/main/Pila.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+/**
+ *
+ * @author SB-C3
+ */
+public class Pila {
+    private int p[];
+    private int tam;
+    private int tope=0;
+    
+    public Pila(int t){
+        tope=tam=t;
+    }
+    
+    public Pila(){
+        tope=tam=0;
+    }
+    
+    //Func
+    public void ingresar(int n){
+        
+    }
+    
+    public void obtener(){
+        
+    }
+    
+    public boolean estaVacia(){
+        return this.tam==0;
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    
+    /*
+        Hacer una interfaz
+        
+        Completar la clase
+    
+    */
+    
+    
+    
+    
+}
