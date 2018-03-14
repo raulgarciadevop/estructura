a26b8b443da8290c7c0e74b23ae57497456861ec
Raul Garcia <33263436+raulgarciadevop@users.noreply.github.com>
2018-03-14 10:57:56 -0700

    Trabajo en clase
    //Tarea: 
    Arbol binario de String.

--- ArbolBinS_ED10_11/src/intt/ArbolBinInt.java
+++ ArbolBinS_ED10_11/src/intt/ArbolBinInt.java
@@ -13,9 +13,11 @@ import static javax.swing.JOptionPane.*;
  */
 public class ArbolBinInt {
     private NodoInt raiz;
+    private String cad;
 
     public ArbolBinInt() {
         raiz=null;
+        cad="";
     }
     
     public void insertar(int d){
@@ -61,14 +63,62 @@ public class ArbolBinInt {
         */ 
     }
     
+    public void preOrden(NodoInt r){
+        if(r==null)
+            return;
+        
+        cad+=r.getInfo()+" - ";
+        preOrden(r.getIzq());
+        preOrden(r.getDer());
+    }
+    
+    public void postOrden(NodoInt r){
+        if(r==null)
+            return;
+        
+        preOrden(r.getIzq());
+        preOrden(r.getDer());
+        cad+=r.getInfo()+" - ";
+    }
+    
+    public void inOrden(NodoInt r){
+        if(r==null)
+            return;
+        
+        preOrden(r.getIzq());
+        cad+=r.getInfo()+" - ";
+        preOrden(r.getDer());
+        
+    }
+    
+    
+    public void verArbol(int tipo){
+        switch(tipo){
+            case 1:
+                preOrden(raiz);
+                break;
+            case 2:
+                postOrden(raiz);
+                break;
+            case 3:
+                inOrden(raiz);
+                break;
+                
+            default:
+                break;
+        }
+        System.out.print(cad);
+    }
+    
     
     public static void main(String[] args){
         ArbolBinInt abi=new ArbolBinInt();
         int e;
         do{
             e=Integer.parseInt(showInputDialog("Ingrese el entero"));
-            
+            abi.insertar(e);
         }while(showConfirmDialog(null, "Otro")==0);
+        abi.verArbol(Integer.parseInt(showInputDialog("Seleccione el ordenamiento:\n\n1.- Pre orden.\n2.- Post orden.\n3.- In orden.")));
     }
      
     
