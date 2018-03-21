5f2e8fdff32284fd0e64c57ffa4bf8807084ed9c
Raul Garcia <33263436+raulgarciadevop@users.noreply.github.com>
2018-03-21 11:09:15 -0700

    Ejercicio y tarea en clase
    //Hacer un grafo de enteros

---
+++ GrafoFlotante/src/main/GrafoFlotante.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+/**
+ *
+ * @author SB-C3
+ */
+public class GrafoFlotante {//      dig |-| . | otro
+    private static final int M[][]={{ 2, 1,-1,-1},//0
+                                    { 2,-1,-1,-1},//1
+                                    { 2,-1, 3,-1},//2
+                                    { 3,-1,-1,-1}};//3
+    
+    
+    public static boolean eside(String p){
+        int edo=0,ent;
+        for(int i=0;i<p.length();i++){
+            int c=p.charAt(i);
+            
+            if(c>=48 && c<=57)//Es digito
+                ent=0;
+            else if(c=='-')
+                ent=1;//1
+            else if(c=='.')
+                ent=2;
+            else 
+                ent=3;
+            
+            edo=M[edo][ent];
+            if(edo==-1)
+                return false;
+            
+            
+        }
+        return true;
+    }
+}
