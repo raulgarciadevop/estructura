805414a2f6cd24e3290010a1d33abda627b0ece3
Raul Garcia <joragarciace@ittepic.edu.mx>
2018-04-17 00:48:15 -0600

    Adelanto del ejercicio de clase
    //Sin terminar por completo

--- BubbleSort/src/main/AlumnoFrame.java
+++ BubbleSort/src/main/AlumnoFrame.java
@@ -20,6 +20,8 @@ public class AlumnoFrame extends javax.swing.JFrame {
     public AlumnoFrame() {
         initComponents();
         dtm=(DefaultTableModel)tblAlumnos.getModel();
+        dtm.setRowCount(0);
+        ord=new Ordenamiento();
     }
 
     /**
@@ -31,13 +33,41 @@ public class AlumnoFrame extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jPanel2 = new javax.swing.JPanel();
         jScrollPane1 = new javax.swing.JScrollPane();
         tblAlumnos = new javax.swing.JTable();
-        jButton1 = new javax.swing.JButton();
+        btnR = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
+        jButton4 = new javax.swing.JButton();
+        jPanel3 = new javax.swing.JPanel();
         jButton3 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jPanel1.setBackground(new java.awt.Color(255, 153, 0));
+
+        jLabel1.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel1.setText("Alumno (Bubble sort)");
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel1)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel1)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
 
         tblAlumnos.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -65,10 +95,10 @@ public class AlumnoFrame extends javax.swing.JFrame {
         });
         jScrollPane1.setViewportView(tblAlumnos);
 
-        jButton1.setText("AddR");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        btnR.setText("AddR");
+        btnR.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                btnRActionPerformed(evt);
             }
         });
 
@@ -79,6 +109,44 @@ public class AlumnoFrame extends javax.swing.JFrame {
             }
         });
 
+        jButton4.setText("Ordenar");
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 375, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jButton4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jButton2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(btnR, javax.swing.GroupLayout.PREFERRED_SIZE, 73, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addComponent(btnR)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jButton2)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jButton4))
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 275, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        jPanel3.setBackground(new java.awt.Color(102, 102, 102));
+
         jButton3.setText("Salir");
         jButton3.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -86,63 +154,99 @@ public class AlumnoFrame extends javax.swing.JFrame {
             }
         });
 
+        javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
+        jPanel3.setLayout(jPanel3Layout);
+        jPanel3Layout.setHorizontalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel3Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jButton3)
+                .addContainerGap())
+        );
+        jPanel3Layout.setVerticalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel3Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jButton3)
+                .addContainerGap())
+        );
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 375, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(jButton2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jButton3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addContainerGap(18, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jPanel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(jButton1)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jButton2)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jButton3))
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 275, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void btnRActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRActionPerformed
+        //Agregar
         dtm.setRowCount(dtm.getRowCount()+1);
-    }//GEN-LAST:event_jButton1ActionPerformed
+    }//GEN-LAST:event_btnRActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // 
+        //Limpiar
         dtm.setRowCount(0);
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        //Salir
         System.exit(0);
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void tblAlumnosMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_tblAlumnosMouseClicked
         // TODO add your handling code here:
-        int tam=dtm.getRowCount();
-        Alumno[] A=new Alumno[tam];
         
-        for(int i=0;i<tam;i++){
-            A[i]=new Alumno(dtm.getValueAt(0, i).toString(),dtm.getValueAt(1, i).toString(),Integer.parseInt(dtm.getValueAt(2, i).toString()),Integer.parseInt(dtm.getValueAt(2, i).toString()));
-        }
     
     
     }//GEN-LAST:event_tblAlumnosMouseClicked
 
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+        // Ordenar
+        add2r();
+    }//GEN-LAST:event_jButton4ActionPerformed
+
+    private void add2r(){
+        int tam=dtm.getRowCount()-1;
+        btnR.setText(String.valueOf(tam));
+        Alumno[] A=new Alumno[dtm.getRowCount()];
+        
+        for(int i=0;i<dtm.getRowCount();i++){
+            A[i]=new Alumno(dtm.getValueAt(0, i).toString(),dtm.getValueAt(1, i).toString(),Integer.parseInt(dtm.getValueAt(2, i).toString()),Integer.parseInt(dtm.getValueAt(3, i).toString()));
+        }//ArrayOutOfBounds
+        
+        /*
+        Exception in thread "AWT-EventQueue-0" java.lang.ArrayIndexOutOfBoundsException: 2 >= 2
+	at java.util.Vector.elementAt(Vector.java:474)
+	at javax.swing.table.DefaultTableModel.getValueAt(DefaultTableModel.java:648)
+	at main.AlumnoFrame.add2r(AlumnoFrame.java:234)
+        */
+        
+        ord.bubble(A,tblAlumnos.getSelectedColumn());
+        
+    }
+    
+    
+    
     /**
      * @param args the command line arguments
      */
@@ -178,11 +282,17 @@ public class AlumnoFrame extends javax.swing.JFrame {
         });
     }
 
-    private DefaultTableModel dtm;
+    private final Ordenamiento ord;
+    private final DefaultTableModel dtm;
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
+    private javax.swing.JButton btnR;
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JPanel jPanel3;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable tblAlumnos;
     // End of variables declaration//GEN-END:variables
--- BubbleSort/src/main/Ordenamiento.java
+++ BubbleSort/src/main/Ordenamiento.java
@@ -21,7 +21,9 @@ public class Ordenamiento {
             }
     */
     
-    public void bubble(int A[]){
+    //BURBUJA
+    
+    public void bubble(int A[]){//Integer
         for(int p=1;p<=A.length;p++)
             for(int i=0;i<A.length-p;i++)
                 if(A[i]>A[i+1]){
@@ -32,6 +34,81 @@ public class Ordenamiento {
         
     }
     
+    public void bubble(String[] A){//String
+        for(int p=1;p<=A.length;p++)
+            for(int i=0;i<A.length-p;i++)
+                if(A[i].compareToIgnoreCase(A[i+1])>0){//checkthis){
+                    String aux=A[i];
+                    A[i]=A[i+1];
+                    A[i+1]=aux;
+                }
+    }
+    
+    public void bubble(Alumno[] A, int c){//Alumno
+        for(int p=1;p<=A.length;p++)
+            for(int i=0;i<A.length-p;i++)
+                switch(c){
+                    case 1://String
+                        if (A[i].getNo_ctrl().compareToIgnoreCase(A[i + 1].getNo_ctrl()) > 0) {//checkthis){
+                            String aux = A[i].getNo_ctrl();
+                            A[i] = A[i + 1];
+                            A[i + 1].setNo_ctrl(aux);
+                        }
+                        break;
+                        
+                    case 2://String
+                        if (A[i].getNombre().compareToIgnoreCase(A[i + 1].getNo_ctrl()) > 0) {//checkthis){
+                            String aux = A[i].getNombre();
+                            A[i] = A[i + 1];
+                            A[i + 1].setNombre(aux);
+                        }
+                        break;
+                        
+                    case 3:
+                        if (A[i].getEdad() > A[i + 1].getEdad()) {
+                            int aux = A[i].getEdad();
+                            A[i] = A[i + 1];
+                            A[i + 1].setEdad(aux);
+                        }
+                        break;
+                        
+                    case 4:
+                        if (A[i].getCalif() > A[i + 1].getCalif()) {
+                            int aux = A[i].getCalif();
+                            A[i] = A[i + 1];
+                            A[i + 1].setCalif(aux);
+                        }
+                        break;
+                        
+                }
+    }
+    
+    
+    //QUICKSORT
+    
+    public void quicksort(int[] A , int i, int f){
+        
+    }
+    
+    public void shell(){
+        
+    }
+    
+    public void radix(){
+        
+    }
+    
+    /*
+    
+    
+    
+                if(A[i].compareToIgnoreCase(A[i+1])>0){//checkthis){
+                    String aux=A[i];
+                    A[i]=A[i+1];
+                    A[i+1]=aux;
+                }
+                
+    
     public void quicksort(int[] A, int i, int f){
         int aux;
         int piv=A[f];
@@ -39,24 +116,11 @@ public class Ordenamiento {
         while(i!=f){
             
         }
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
         //if(i==f)
           //  return;
         //else{
           //  piv=A[f];
         //}
-        
-        
-        
         while(A[i]<= piv){
             i++;
             aux=A[i];
@@ -72,11 +136,6 @@ public class Ordenamiento {
             
         }
         
-        
-        
-            
-        
-        /*
         while(i<f){
             
             while(A[i]<=piv && i<f)
@@ -87,8 +146,8 @@ public class Ordenamiento {
         }
         
         if()
-        */
+        
             
     }
-    
+    */
 }
